a67a447033ec9e18c8e6321a39f7549a57e7abcd
WUlLlLEP <maildlyaps4@mail.ru>
2020-11-18 15:30:46 +0300

    Updates

---
+++ precache-manifest.3a3d0a19b845685c06f3d587dbb209ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79c2577ed8a977bfdd681d086c9e1b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b66f1902f8332bd8f7ee",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7e5191c98298150397b5",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b66f1902f8332bd8f7ee",
+    "url": "/static/js/2.c4fadd3d.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.c4fadd3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5191c98298150397b5",
+    "url": "/static/js/main.8079d85c.chunk.js"
+  },
+  {
+    "revision": "d94af4ebe80c98d5c78d",
+    "url": "/static/js/runtime-main.ae4caf45.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
