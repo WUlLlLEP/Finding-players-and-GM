3dab30b89c56358f7e1d5eab38a1e218421b9b97
WUlLlLEP <maildlyaps4@mail.ru>
2020-11-18 15:34:11 +0300

    Updates

---
+++ precache-manifest.d9a56859908f6357b6199d3695ae5fed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df2ab894ddcf174d3828a198ef0bd8cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4ef2a73a6956f25f35a9",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7e5191c98298150397b5",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4ef2a73a6956f25f35a9",
+    "url": "./static/js/2.87034450.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.87034450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5191c98298150397b5",
+    "url": "./static/js/main.8079d85c.chunk.js"
+  },
+  {
+    "revision": "b612f7bc6b7d5aacad10",
+    "url": "./static/js/runtime-main.51bcb6e0.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3a3d0a19b845685c06f3d587dbb209ce.js"
+  "./precache-manifest.d9a56859908f6357b6199d3695ae5fed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
